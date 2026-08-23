34c149834b37483b8138d857a420c6f92d944276
Nilza-Kelly <nilzakelly@aluno.unifapce.edu.br>
2026-08-23 18:05:36 -0300

    Implementa desafio de conjuntos

--- Aula02-Exercícios/04-implementando-conjuntos/desafio-pratico1.py
+++ Aula02-Exercícios/04-implementando-conjuntos/desafio-pratico1.py
@@ -1,1 +1,23 @@
+class MeuConjunto :
+    def __init__ ( self , elementos ) :
+        self . dados = []
+        for e in elementos :
+            if e not in self . dados :
+                self . dados . append ( e )
 
+    def uniao ( self , outro ) :
+        resultado = self . dados . copy ()
+        for e in outro . dados :
+            if e not in resultado :
+                resultado . append ( e )
+        return MeuConjunto ( resultado )
+
+    def intersecao ( self , outro ) :
+        resultado = [ e for e in self . dados if e in outro . dados ]
+        return MeuConjunto ( resultado )
+
+A = MeuConjunto ([1 , 2 , 2 , 3]) 
+B = MeuConjunto ([3 , 4 , 5])
+
+print ( " Uniao : " , A . uniao ( B ) . dados ) 
+print ( " Intersecao : " , A . intersecao ( B ) . dados )
